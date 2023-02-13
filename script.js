9908bf4b4a95ee2805c1399992ac05197ee5e9fa
DevMwalker <125302164+DevMWalker@users.noreply.github.com>
2023-02-13 12:19:48 -0600

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,31 @@
+let button1=document.querySelector(".option-one");
+let screen1=document.querySelector(".option-one-screen");
+
+let button2=document.querySelector(".option-two");
+let screen2=document.querySelector(".option-two-screen");
+
+let buttion3=document.querySelector(".option-three");
+let screen3=document.querySelector(".option-one-end");
+
+let buttion4=document.querySelector(".option-four");
+let screen4=document.querySelector(".option-two-end");
+
+//* When you're ready to make event handlers, uncomment the code below. Then fill in the blanks with the correct variables!
+button1.onclick=function(){
+screen1.style.display="block";
+};
+
+button2.onclick=function(){
+screen2.style.display="block";
+};
+
+
+buttion3.onclick=function(){
+screen3.style.display="block";
+};
+
+buttion4.onclick=function(){
+screen4.style.display="block";
+};
+
+
